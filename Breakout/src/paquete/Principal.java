25b3cd57a1d99a12908b926aad1eb72dcb477fa5
USER <USER@christian>
2018-01-17 08:07:35 -0500

    Esta echo el grafico

--- Breakout/src/paquete/Principal.java
+++ Breakout/src/paquete/Principal.java
@@ -1,9 +1,20 @@
 
 package paquete;
 
+import Vista.PanelInicial1;
+import javax.swing.JFrame;
+
 public class Principal {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        JFrame ventana = new JFrame();
+        ventana.setTitle("BrakeOut");
+        ventana.setSize(800, 500);
+        PanelInicial1 pI=new PanelInicial1();
+        ventana.setContentPane(pI);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
         
     }
     
